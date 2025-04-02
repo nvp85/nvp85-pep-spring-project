b4a1822edd772ac1d3b31b95d2ed91f03650e308
nvp85 <nvp@zhukov.al>
2025-04-01 22:36:48 -0500

    update MessageService, annotate it as a bean

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,20 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+import com.example.entity.*;
+import com.example.repository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
 public class MessageService {
+
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
 }
