24d6cd2d04ae7931fe784b9844fbe054f25e7cae
Natalia Plishkina <nvp@zhukov.al>
2025-04-03 00:56:37 +0000

    add the check if the text's length is appropriate

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -63,6 +63,9 @@ public class SocialMediaController {
     @PostMapping("/messages")
     @ResponseBody
     public ResponseEntity<Message> postMessage(@RequestBody Message newMessage) throws BadRequestException {
+        if (newMessage.getMessageText().length() == 0 || newMessage.getMessageText().length() > 255) {
+            throw new BadRequestException("Failed to create a new message");
+        }
         Message message = messageService.createMessage(newMessage);
         if (message == null) {
             throw new BadRequestException("Failed to create a new message");
